92a850dafde7e410c7337f25616da85dc0b29c93
alphabet5 <alphabet5@users.noreply.github.com>
2020-04-15 15:29:26 -0500

    Initial commit.

---
+++ DisableWindowsKey.ps1
@@ -0,0 +1,43 @@
+$code = @'
+    [DllImport("user32.dll")]
+     public static extern IntPtr GetForegroundWindow();
+'@
+Add-Type $code -Name Utils -Namespace Win32
+$settings = Import-Csv -Path .\WindowsKeyGuac_settings.txt
+$start_count = 0
+$stop_count = 0
+$registry_path = "Registry::HKCU\Software\Microsoft\Windows\CurrentVersion\Policies\Explorer"
+if ((Get-Item -Path $registry_path).GetValue("NoWinKeys") -eq $null){
+    New-ItemProperty -Path $registry_path -Name NoWinKeys -Value 0 -PropertyType DWORD -Force | Out-Null
+}
+
+while(1){
+    $hwnd = [Win32.Utils]::GetForegroundWindow()
+    $process = Get-Process | Where-Object { $_.mainWindowHandle -eq $hwnd }
+    $guac_not_running = $true
+    ForEach ($row in $settings) {
+        if ($process.ProcessName -eq $row.ProcessName -and $process.MainWindowTitle -like $row.MainWindowTitle){
+            $start_count += 1
+            if ($start_count -gt 20){
+                if ((Get-ItemPropertyValue $registry_path -Name NoWinKeys) -ne 1){
+                    New-ItemProperty -Path $registry_path -Name NoWinKeys -Value 1 -PropertyType DWORD -Force | Out-Null
+                    Stop-Process -ProcessName explorer
+                    sleep 2
+                }
+            }
+            $guac_not_running = $false
+            $stop_count = 0
+        }
+    }
+    if ($guac_not_running){
+    $start_count = 0
+    $stop_count += 1
+    }
+    if ($stop_count -gt 20){
+        if ((Get-ItemPropertyValue $registry_path -Name NoWinKeys) -ne 0){
+            New-ItemProperty -Path $registry_path -Name NoWinKeys -Value 0 -PropertyType DWORD -Force | Out-Null
+            Stop-Process -ProcessName explorer
+        }
+    }
+    sleep -Milliseconds 100
+}
--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # DisableWindowsKey
  A script to disable the windows key when specific applications and/or window titles are in the foreground.
+
+## Overview
+
+This script was created to allow users to easily / automatically modify their windows hotkey settings for sending those hotkeys to apache guacamole.
+
+## Steps to use
+
+> Download the .ps1 file.
+> Update the .txt file with process names that you want to disable the windows hotkeys when they're in the foreground.
+> Update the .txt with window titles that you want to disable the windows hotkeys when the window is in the foreground.
+
+## Exe usage.
+> Download the .exe file.
+> Update the .txt file with process names that you want to disable the windows hotkeys when they're in the foreground.
+> Update the .txt with window titles that you want to disable the windows hotkeys when the window is in the foreground.
